3a8dd6b1a791f821cd239aad59f96b6f39c99e9d
yjspoder <injose.joshi@gmail.com>
2020-04-22 00:06:18 +0530

    Added tasks controller and index action

---
+++ db/migrate/20200421151634_change_description_type_to_text.rb
@@ -0,0 +1,9 @@
+class ChangeDescriptionTypeToText < ActiveRecord::Migration[6.0]
+  def up
+    change_column :tasks, :description, :text
+  end
+
+  def down
+    change_column :tasks, :description, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_214049) do
+ActiveRecord::Schema.define(version: 2020_04_21_151634) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "description"
