f721212dfc2cd72d429a621531182dd28513651f
yjspoder <injose.joshi@gmail.com>
2020-04-22 00:57:42 +0530

    Added constraint to make description not nullable

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_151634) do
+ActiveRecord::Schema.define(version: 2020_04_21_192441) do
 
   create_table "tasks", force: :cascade do |t|
-    t.text "description"
+    t.text "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
