b32dde04795a7fe268b1873c804fe5ac8cbef820
yjspoder <injose.joshi@gmail.com>
2020-04-24 16:42:47 +0530

    Add edit link to Show.js

--- app/javascript/components/tasks/Show.js
+++ app/javascript/components/tasks/Show.js
@@ -8,8 +8,14 @@ class Show extends Component {
         <div className="container">
           <h2 className="py-3">You just created a new task!</h2>
           <div className="row">
-            <div className="col-md-10">
+            <div className="col-md-10 font-weight-bold">
               {task.id}.{task.description}
+              <a
+                className="ml-2 btn btn-sm btn-warning"
+                href={Routes.edit_task_path(task.id)}
+              >
+                Edit
+              </a>
             </div>
           </div>
         </div>
