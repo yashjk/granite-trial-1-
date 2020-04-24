d46b04a15e332b9cf280010aee113df730d8a4ce
yjspoder <injose.joshi@gmail.com>
2020-04-24 17:37:41 +0530

    RefactAPI.js

--- app/javascript/utils/API.js
+++ app/javascript/utils/API.js
@@ -1,21 +1,57 @@
-const headers = {
-    'Accept' : 'application/json',
-    'Content-Type' : 'application/json',
-    'X-CSRF-TOKEN': document.querySelector('[name="csrf-token"]').content
-};
+import { isUnauthorized } from "./helpers";
+import {
+  STATUS_UNPROCESSABLE_ENTITY,
+  STATUS_OK,
+  STATUS_NOT_FOUND,
+  STATUS_BAD_REQUEST,
+} from "./constants";
 
-export default {
-    postNewTask: payload => {
-        return fetch(Routes.tasks_path(), {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(payload), 
-        })
-        .then(response => {
-            if(!response.ok){
-                throw response
-            }
-            return response.json();
-        })
-    }
-}
+export const fetchApi = ({
+  url,
+  method,
+  body,
+  onError,
+  errorCallback,
+  onSuccess,
+  successCallBack,
+}) => {
+  method = method.toUpperCase();
+
+  var options = {
+    method: method,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-CSRF-TOKEN": document.querySelector('[name="csrf-token"]').content,
+    },
+  };
+
+  const methods = ["POST", "PUT", "PATCH"];
+  if (methods.includes(method)) {
+    options["body"] = JSON.stringify(body);
+  }
+
+  fetch(url, options)
+    .then((response) =>
+      response.json().then((data) => {
+        if (isUnauthorized(response)) {
+          onError({ messages: [data.errors], type: "danger" });
+        } else if (response.status == STATUS_UNPROCESSABLE_ENTITY) {
+          onError({ messages: data.errors, type: "danger" });
+        } else if (
+          response.status >= STATUS_OK &&
+          response.status < STATUS_BAD_REQUEST
+        ) {
+          onSuccess({ messages: [data.notice], type: "success" });
+          successCallBack(data);
+        } else if (response.status == STATUS_NOT_FOUND) {
+          onError({ messages: [data.errors], type: "danger" });
+        } else {
+          throw Error(response.statusText);
+        }
+      })
+    )
+    .catch((error) => {
+      console.error(error);
+    });
+};
