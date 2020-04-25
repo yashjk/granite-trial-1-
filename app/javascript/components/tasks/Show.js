a598a1ed5604edc6604d65e6f42c86ce6b620081
yjspoder <injose.joshi@gmail.com>
2020-04-26 02:26:02 +0530

    Add handleDelete and handleError to Show.js

--- app/javascript/components/tasks/Show.js
+++ app/javascript/components/tasks/Show.js
@@ -1,6 +1,42 @@
 import React, { Component } from "react";
+import { fetchApi } from "../../utils/API";
 
 class Show extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: null,
+    };
+    this.handleError = this.handleError.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleError = (response) => {
+    this.setState({
+      errors: {
+        errors: response.messages,
+        type: response.type,
+      },
+    });
+  };
+
+  handleDelete = (taskId) => {
+    let taskDelete = confirm("Are you sure you want to delete the task?");
+    if (taskDelete) {
+      fetchApi({
+        url: Routes.task_path(taskId),
+        method: "DELETE",
+        onError: this.handleError,
+        onSuccess: (response) => {
+          console.log(response);
+        },
+        successCallBack: () => {
+          window.location.replace(Routes.tasks_path());
+        },
+      });
+    }
+  };
+
   render() {
     const { task } = this.props;
     return (
@@ -16,7 +52,12 @@ class Show extends Component {
               >
                 Edit
               </a>
-              <a className="ml-2 btn btn-sm btn-warning">Delete</a>
+              <a
+                className="ml-2 btn btn-sm btn-warning"
+                onClick={() => this.handleDelete(task.id)}
+              >
+                Delete
+              </a>
             </div>
           </div>
         </div>
