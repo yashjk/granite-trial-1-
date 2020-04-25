0d152c3523560a942a63b733918e727e1a98a489
yjspoder <injose.joshi@gmail.com>
2020-04-25 22:14:05 +0530

    Add delete to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tasks, only: [:index, :new, :create, :show, :edit, :update]
+  resources :tasks, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
 end
