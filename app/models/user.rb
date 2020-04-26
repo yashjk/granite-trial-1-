8f96d261b2bfcc12f908920a77bc8b082317d25c
yjspoder <injose.joshi@gmail.com>
2020-04-26 21:30:32 +0530

    Add email and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy, foreign_key: :assignee_id
   validates :name, presence: true, uniqueness: true
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
