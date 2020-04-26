a0535a5e8f09329e1db1c9c15f5935729e97d518
yjspoder <injose.joshi@gmail.com>
2020-04-26 17:18:44 +0530

    Add association between user and task models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
+  belongs_to :user, foreign_key: :assignee_id
   validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  has_many :tasks, dependent: :destroy, foreign_key: :assignee_id
 end
