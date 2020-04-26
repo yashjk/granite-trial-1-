c889ce1982182599efcc7a580184dc80ee048b2a
yjspoder <injose.joshi@gmail.com>
2020-04-26 19:51:36 +0530

    Refactor New.js to assign user to task

--- app/javascript/components/tasks/New.js
+++ app/javascript/components/tasks/New.js
@@ -1,14 +1,17 @@
-//chapter 9 Displaying errors, props should be errors={errors.error}
 import React, { Component } from "react";
 import { fetchApi } from "../../utils/API";
-import Errors from "../shared/Errors";
+import Routes from "../../js-routes.js.erb";
 
 class New extends Component {
   constructor(props) {
+    console.log(props);
     super(props);
     this.state = {
-      description: "",
-      errors: null,
+      task: {
+        description: "",
+        assignee_id: "1",
+        errors: null,
+      },
     };
 
     this.onSubmit = this.onSubmit.bind(this);
@@ -21,10 +24,10 @@ class New extends Component {
       url: Routes.tasks_path(),
       method: "POST",
       body: {
-        task: { description: this.state.description },
+        task: this.state.task,
       },
       onError: (response) => {
-        console.log(response);
+        console.error(response);
       },
       onSuccess: (response) => {
         console.log(response);
@@ -35,24 +38,19 @@ class New extends Component {
     });
   }
 
-  handleChange(event) {
+  handleChange = ({ target: { name, value } }) => {
     this.setState({
-      description: event.target.value,
+      task: {
+        ...this.state.task,
+        [name]: value,
+      },
     });
-  }
+  };
 
   render() {
-    const { errors } = this.state;
+    const { users } = this.props;
     return (
       <div className="container">
-        <div className="row justify-content-center">
-          {errors && errors.length != 0 ? (
-            <div className="mt-4">
-              <Errors errors={errors.error} message="danger" />
-            </div>
-          ) : null}
-        </div>
-
         <div className="col-md-10 mx-auto pt-2">
           <div className="row">
             <h3 className="pb-3">Add Task</h3>
@@ -67,8 +65,30 @@ class New extends Component {
                   type="text"
                   className="form-control"
                   onChange={this.handleChange}
+                  name="description"
                   value={this.state.description}
+                  required
                 />
+              </div>
+            </div>
+            <div className="form-group row pt-3">
+              <label htmlFor="name" className="col-sm-2 col-form-label">
+                <h5 className="text-secondary ">Assigned to: </h5>
+              </label>
+              <div className="col-sm-10">
+                <select
+                  className="custom-select"
+                  name="assignee_id"
+                  id="users"
+                  onChange={this.handleChange}
+                >
+                  {users &&
+                    users.map((user) => (
+                      <option value={user.id} key={user.id}>
+                        {user.name}
+                      </option>
+                    ))}
+                </select>
               </div>
             </div>
             <div className="form-group row pt float-right pr-3">
