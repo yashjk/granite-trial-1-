ae02da6770176981ee3201436934a67c0e31d1e3
yjspoder <injose.joshi@gmail.com>
2020-04-26 21:15:04 +0530

    Add email and password digest to migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_105604) do
+ActiveRecord::Schema.define(version: 2020_04_26_154006) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "description", null: false
