497bb8a9e67fe2ff155883ac670e79a0cb8a4d1c
yjspoder <injose.joshi@gmail.com>
2020-04-27 18:22:19 +0530

    Add new and create method to sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+    render
+  end
+
+  def create
+    user = User.find_by(email: params[:login][:email].downcase)
+    if user && user.authenticate(params[:login][:password])
+      session[:user_id] = user.id.to_s
+      flash[:success] = "Successfully logged in!"
+
+      render status: :ok, json: { notice: 'Successfully logged in' }
+    else
+      render status: :not_found, json: { notice: 'Invalid credentials, try again' }
+    end
+  end
+  
+end
