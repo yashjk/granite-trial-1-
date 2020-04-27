bfd81458496ac064ec651dbfac09a6d9eb5b60d7
yjspoder <injose.joshi@gmail.com>
2020-04-27 18:22:42 +0530

    Add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-
   root to: 'users#new'
-
   resources :users, only: [:new, :create]
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
 
 end
