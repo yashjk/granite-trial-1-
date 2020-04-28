86fa6547504caf1f5a9ea70a82902ec7ea3c7e1e
yjspoder <injose.joshi@gmail.com>
2020-04-28 16:16:07 +0530

    Add user not authorized to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :logged_in?, :current_user
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 
@@ -24,6 +25,11 @@ class ApplicationController < ActionController::Base
     if session[:user_id]
       @current_user ||= User.find_by(session[:user_id])
     end
+  end
+
+  def user_not_authorized
+    flash[:warning] = "All accessable tasks are listed below."
+    redirect_to root_path
   end
         
 end
