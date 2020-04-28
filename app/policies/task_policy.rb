51dea9fef65626da4c25f51ab1edc9545cd732ef
yjspoder <injose.joshi@gmail.com>
2020-04-28 15:37:44 +0530

    Add edit?,  update?,  create? and destroy? methods

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -7,6 +7,23 @@ class TaskPolicy
   end
 
   def show?
+    task.creator_id == user.id || task.asignee_id == user.id
   end
 
+  def edit?
+    show?
+  end
+
+  def update?
+    show?
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    task.creator_id == user.id
+  end
+  
 end
