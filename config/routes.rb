5ea264a707103a746d62c76c9afa1abf288c369d
yjspoder <injose.joshi@gmail.com>
2020-04-29 06:21:01 +0530

    Add route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tasks, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :tasks do
+    resources :status, only: [:update]
+    resources :comments, only: [:create]
+  end
+
   root to: 'users#new'
   resources :users, only: [:new, :create]
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+
 end
