46215f8bfe7a39af8d9d72f21f6e80a4e87cd2b5
yjspoder <injose.joshi@gmail.com>
2020-04-29 06:16:52 +0530

    Add comments to user and task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user, foreign_key: :assignee_id
+  has_many :comments, dependent: :destroy
   validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy, foreign_key: :assignee_id
+  has_many :comments, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   has_secure_password
   validates :email, presence: true, uniqueness: true
